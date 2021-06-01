dfc2b0bd9635594848f80f9f12405feb6f2552f7
Malik Umair Khalid <fighteromega3462@gmail.com>
2021-06-01 17:28:09 +0500

    question 1-3 completed

---
+++ app.js
@@ -0,0 +1,69 @@
+// *** Q 1 *** //
+
+// function tellTime() {
+// var now = new Date();
+// var theHr = now.getHours();
+// var theMin = now.getMinutes();
+// alert("Current time: "+ theHr + ":" + theMin);
+// }
+// alert(tellTime())
+
+// *** Q 2 *** //
+
+// var userInput1 = +prompt("enter no 1")
+// var userInput2 = +prompt("enter no 2")
+
+// function adder(){
+// alert(userInput1 + userInput2)
+// }
+// alert(adder())
+
+
+// *** Q 3 *** //
+
+// var no1 = +prompt("Enter no 1")
+// var no2 = +prompt("Enter no 2")
+// var operation = prompt("which operation do you wanut")
+
+// function calc () {
+//     if(operation === "+"){
+//     alert (no1 + no2)
+// }
+//     else if(operation === "-"){
+//     alert (no1 - no2)
+// }
+//     else if(operation === "*"){
+//     alert (no1 * no2)
+// }
+//     else if(operation === "/"){
+//     alert (no1 / no2)
+// }
+//     else{
+//         alert("Invalid choise")
+//     }
+// }
+// alert (calc())
+
+// *** Q 4 *** //
+
+
+
+// *** Q 1 *** //
+// *** Q 1 *** //
+
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+// *** Q 1 *** //
+
+// *** Q 1 *** //
